12de3d6d0e3baed9a39e7d8e13c01e3b0b0526b4
Md Firdous <firdousraza080@gmail.com>
2024-02-15 17:11:15 +0530

    first commit

---
+++ add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter a : ");
+    scanf("%d",&a);
+
+    int b;
+    printf("Enter b : ");
+    scanf("%d",&b);
+
+    printf("%d",a+b);
+
+    return 0;
+}
---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    scanf("%d",&a);
+
+    int b;
+    scanf("%d",&b);
+
+    printf("%d",a+b);
+
+    return 0;
+}
